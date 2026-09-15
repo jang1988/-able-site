2beef806b161494fac586633813d09ef683a8aa4
jang1988 <pustota1988@gmail.com>
2026-09-15 10:20:42 +0300

    robotsSitemaoCreate

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://bigbudcom.net/sitemap.xml',
+  }
+}
---
+++ app/sitemap.js
@@ -0,0 +1,52 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://bigbudcom.net',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://bigbudcom.net/o-nas',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://bigbudcom.net/oferta',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://bigbudcom.net/sprzet',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://bigbudcom.net/galeria',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://bigbudcom.net/referencje',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://bigbudcom.net/kontakt',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://bigbudcom.net/rodo',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.3,
+    },
+  ]
+}
